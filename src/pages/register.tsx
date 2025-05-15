3411f683ab9afd8d18fda54b95d15c7cdb134002
Mark Nazzaro <marknazzaro2@gmail.com>
2025-05-15 02:20:27 -0400

    No register

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -27,54 +27,6 @@ const Register = () => {
 
   return (
     <RequireNoAuth>
-      <div className="container flex flex-col mb-8">
-        <div className="lg:w-2xl sm:mx-auto">
-          <div className="text-center mb-8">
-            <h1 className="font-heading text-4xl mb-2 dark:text-white">
-              Sign up
-            </h1>
-            <Link to="/login" className="text-green hover:underline">
-              Have an account ?
-            </Link>
-          </div>
-          <FormValidation className="flex flex-col gap-4" action={submit}>
-            <div>
-              <input
-                className="form-control"
-                type="text"
-                placeholder="Your Name"
-                required
-                value={form.username}
-                onChange={(e) => setForm({ ...form, username: e.target.value })}
-              />
-            </div>
-            <div>
-              <input
-                type="email"
-                placeholder="Email"
-                className="form-control"
-                required
-                value={form.email}
-                onChange={(e) => setForm({ ...form, email: e.target.value })}
-              />
-            </div>
-            <div>
-              <input
-                type="password"
-                placeholder="Password"
-                className="form-control"
-                required
-                value={form.password}
-                onChange={(e) => setForm({ ...form, password: e.target.value })}
-              />
-            </div>
-
-            <div className="flex justify-end">
-              <BaseButton>Sign up</BaseButton>
-            </div>
-          </FormValidation>
-        </div>
-      </div>
     </RequireNoAuth>
   );
 };
