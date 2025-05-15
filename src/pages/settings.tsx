e4c97e1da74d34a4644d86f3613510ddc5f091c3
Mark Nazzaro <marknazzaro2@gmail.com>
2025-05-15 00:21:13 -0400

    No settings page

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -44,65 +44,7 @@ const Settings = () => {
 
   return (
     <RequireAuth>
-      <div className="container flex flex-col mb-8">
-        <div className="lg:w-2xl sm:mx-auto">
-          <div className="text-center mb-8">
-            <h1 className="font-heading text-4xl dark:text-white">
-              Your settings
-            </h1>
-          </div>
-          <FormValidation className="flex flex-col gap-4" action={submit}>
-            <>
-              {success && (
-                <SuccessMessage>
-                  Your settings have been updated successfully
-                </SuccessMessage>
-              )}
-            </>
-            <div>
-              <input
-                className="form-control"
-                type="text"
-                placeholder="URL of profile picture"
-                value={form.image}
-                onChange={(e) => setForm({ ...form, image: e.target.value })}
-              />
-            </div>
-            <div>
-              <input
-                className="form-control"
-                type="text"
-                placeholder="Your Name"
-                required
-                value={form.username}
-                onChange={(e) => setForm({ ...form, username: e.target.value })}
-              />
-            </div>
-            <div>
-              <textarea
-                placeholder="Short bio about you"
-                className="form-control"
-                value={form.bio}
-                onChange={(e) => setForm({ ...form, bio: e.target.value })}
-              />
-            </div>
-            <div>
-              <input
-                type="email"
-                placeholder="Email"
-                className="form-control"
-                required
-                value={form.email}
-                onChange={(e) => setForm({ ...form, email: e.target.value })}
-              />
-            </div>
-
-            <div className="flex justify-end">
-              <BaseButton>Update Settings</BaseButton>
-            </div>
-          </FormValidation>
-        </div>
-      </div>
+      
     </RequireAuth>
   );
 };
